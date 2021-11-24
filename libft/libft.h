3bc09b4c2f802d16183d4352e54f8e5b31a1fd16
Daniel Oliveira <dtoliverbr@gmail.com>
2021-11-24 04:29:25 +0000

    memcmp.c

--- libft/libft.h
+++ libft/libft.h
@@ -21,6 +21,7 @@ void ft_bzero(void*, size_t);
 void *ft_memcpy(void*, const void*, size_t);
 void *ft_memmove(void*, const void*, size_t);
 void *ft_memchr(const void*, int, size_t);
+int ft_memcmp(const void*, const void*, size_t);
 
 
 // String manipulation funcios
