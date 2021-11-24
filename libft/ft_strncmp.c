b05ffc3466288b9d1362507af98e6c097125b0a3
Daniel Oliveira <dtoliverbr@gmail.com>
2021-11-24 03:22:39 +0000

    strncmp.c

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 02:58:29 by dateixei          #+#    #+#             */
+/*   Updated: 2021/11/24 02:58:29 by dateixei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_strncmp(const char *s1, const char *s2, size_t n) {
+	size_t i;
+
+	i = 0;
+	while(n > i && (s1[i] != '\0' || s2[i] != '\0')) {
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return 0;
+}
+// int main() {
+// 	char s1[] = "s1 string";
+// 	char s2[] = "s2 string";
+// 	int orig;
+// 	int ft;
+
+// 	printf("%d\n", orig = strncmp(s1, s2, 2));
+// 	printf("%d\n", ft = ft_strncmp(s1, s2, 2));
+	
+
+// 	return 0;
+// }
--- libft/libft.h
+++ libft/libft.h
@@ -30,5 +30,6 @@ int ft_toupper(int);
 int ft_tolower(int);
 char *ft_strchr(const char*, int);
 char *ft_strrchr(const char*, int);
+int ft_strncmp(const char*, const char*, size_t);
 
 #endif
