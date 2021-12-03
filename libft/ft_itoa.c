d17982dcbadb3fe6d376e5321c315bd0eed75d43
Daniel Oliveira <dtoliverbr@gmail.com>
2021-12-03 01:22:25 +0000

    itoa

---
+++ libft/ft_itoa.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 00:31:48 by dateixei          #+#    #+#             */
+/*   Updated: 2021/12/03 00:41:42 by dateixei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// Allocates (with malloc(3)) and returns a string
+// representing the integer received as an argument.
+// Negative numbers must be handled.
+
+char *ft_itoa(int n)
+{
+	char	*itoa;
+
+	
+}
