da69aec7592b607c4861fdf8391e45eca42bc4b1
Daniel Oliveira <dtoliverbr@gmail.com>
2021-12-10 23:13:14 +0000

    get_next_line

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/09 01:33:56 by dateixei          #+#    #+#             */
+/*   Updated: 2021/12/09 01:33:57 by dateixei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+// Write a function which returns a line read from a
+// file descriptor
+
+char *get_next_line(int fd)
+{
+
+}
