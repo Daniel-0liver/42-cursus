cdb93a46f9333651bab8cef15bf002b05fab0850
Daniel Oliveira <dtoliverbr@gmail.com>
2022-02-21 00:57:37 +0000

    bonus_part

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 00:49:18 by dateixei          #+#    #+#             */
+/*   Updated: 2022/02/21 00:55:59 by dateixei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <stdlib.h>
+
+
+
+# endif
