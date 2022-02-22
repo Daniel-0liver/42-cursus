673e7f1ee6f0be3f1ab4c1f20a429178904f45a9
Daniel Oliveira <dtoliverbr@gmail.com>
2022-02-22 02:00:12 +0000

    Update

--- libft/main.c
+++ libft/main.c
@@ -2,9 +2,16 @@
 
 int main() {
 	char s1[] = "aHere is a very big test but noa";
-	char s2[] = "aH";
-	char *s3;
-	s3 = ft_strtrim(s1, s2);
-	printf("%s", s3);
+	char s2 = ' ';
+	char **s3;
+	int i = 0;
+	int j = 0;
+	s3 = ft_split(s1, s2);
+	while (s3[i])
+	{
+		printf("%s\n", s3[i]);	
+		i++;
+	}
+	
 	return (0);
 }
