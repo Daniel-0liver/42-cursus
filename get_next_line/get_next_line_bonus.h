e9ba582108d15e004d88cb06399d39ed9f6f0ff7
Daniel Oliveira <dtoliverbr@gmail.com>
2022-03-06 23:29:04 +0000

    Updates

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 00:49:18 by dateixei          #+#    #+#             */
-/*   Updated: 2022/02/24 01:28:14 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/03/06 18:19:49 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@
 #include <stdlib.h>
 
 char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+void	*ft_calloc(size_t sizeOne, size_t sizeTwo);
 
 # endif
--- get_next_line/get_next_line_utils_bonus.c
+++ get_next_line/get_next_line_utils_bonus.c
@@ -6,13 +6,13 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 00:49:21 by dateixei          #+#    #+#             */
-/*   Updated: 2022/03/03 00:10:53 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/03/06 18:19:56 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-void	*ft_calloc(size_t	sizeOne, size_t sizeTwo)
+void	*ft_calloc(size_t sizeOne, size_t sizeTwo)
 {
 	char	*str;
 	void	*str_return;
@@ -26,4 +26,14 @@ void	*ft_calloc(size_t	sizeOne, size_t sizeTwo)
 	while (full_size--)
 		str++ = (char) 0;
 	return (str_return);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i)
 }
