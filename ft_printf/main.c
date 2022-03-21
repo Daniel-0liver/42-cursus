e9be1f322f4adb5f7e52f475ca89228f8518a7ef
Daniel Oliveira <dtoliverbr@gmail.com>
2022-03-21 02:37:40 +0000

    main printf

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -1,20 +1,38 @@
-#include "ft_printf.h"
-#include "libft.h"
 #include <stdarg.h>
 #include <stdio.h>
 
 int	ft_printf(const char *str, ...)
 {
+	char *str_args;
 	int	num_args; 
-	int	i;
+	char char_args;
+	int i;
+	char	c;
 	va_list args;
 
-	num_args = ft_strlen(str);
 	va_start(args, str);
 	i = 0;
 	while (str[i])
 	{
-		ft_putchar_fd(str[i], 1);
+		c = str[i + 1];
+		if(c == 'd')
+		{
+			num_args = va_arg(args, int);
+			printf("%d\n", num_args);
+		}
+		else if(c == 'c')
+		{
+			char_args = va_arg(args, int);
+			printf("%c\n", char_args);
+		}
+		else if(c == 's')
+		{
+			str_args = va_arg(args, char*);
+			printf("%s\n", str_args);
+		} 
+		else
+			printf("%c", str[i]);
+		i++;
 	}
 	va_end(args);
 	return 0;
@@ -22,6 +40,6 @@ int	ft_printf(const char *str, ...)
 
 int main(void)
 {
-	ft_printf("testando");
+	ft_printf("te%c%stan%do", 'c', "test", 5);
 	return 0;
 }	
