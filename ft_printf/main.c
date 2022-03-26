de81b86ceb131960519d8a489b4a1d17d7e746a4
Daniel Oliveira <dtoliverbr@gmail.com>
2022-03-26 23:44:16 +0000

    printf start

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -2,23 +2,5 @@
 
 int main()
 {
-	// unsigned int n;
-	unsigned int n2;
-	int *i;
-	char *str;
-	char **str1;
-	unsigned int t;
-	// char test[1000];
-	str = "teste";
-
-	t = &str;
-
-	printf("%p\n%u", &str, t);
-	
-
-	// n = -54;
-	// i = -54;
-	// n2 = -55;
-	// printf("%d\n%u\n%u", i, n, n2);
 	return 0;
 }
