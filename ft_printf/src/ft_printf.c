07411594bc15738b3b7188d2c1472f03714b7de8
Daniel Oliveira <dtoliverbr@gmail.com>
2022-03-29 23:12:17 +0100

    I really need to finish this project soon

--- ft_printf/src/ft_printf.c
+++ ft_printf/src/ft_printf.c
@@ -6,13 +6,14 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 23:51:49 by dateixei          #+#    #+#             */
-/*   Updated: 2022/03/27 23:53:11 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/03/29 23:11:31 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printf(const char *, ...)
+int ft_printf(const char *printf, ...)
 {
 	//starting the printf
+	//I really need to start this project
 }
