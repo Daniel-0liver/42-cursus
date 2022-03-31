7beac944ac2effda8d6cee2759f571eb46181647
Daniel Oliveira <dtoliverbr@gmail.com>
2022-03-31 01:20:44 +0100

    Initial tests

--- ft_printf/src/ft_printf.c
+++ ft_printf/src/ft_printf.c
@@ -6,14 +6,29 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 23:51:49 by dateixei          #+#    #+#             */
-/*   Updated: 2022/03/29 23:11:31 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/03/31 01:09:20 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+// #include "ft_printf.h"
+#include <stdio.h>
 
-int ft_printf(const char *printf, ...)
+int ft_printf(const char *str, ...)
 {
-	//starting the printf
-	//I really need to start this project
+	char	*test;
+	
+	va_list args;
+	va_start(args, str);
+	test = va_arg(args, char);
+	printf("%s\n", test);
+	va_end(args);
+	return 0;
+}
+
+int main()
+{
+	int i;
+
+	i = ft_printf("aas", "Testing");
+	return 0;
 }
