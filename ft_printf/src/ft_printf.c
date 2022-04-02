2f10ac1b444f02fec9ec496d01e15c8cd0f61bfb
Daniel-0liver <dtoliverbr@gmail.com>
2022-04-02 12:38:13 +0100

    ft_printf_d

--- ft_printf/src/ft_printf.c
+++ ft_printf/src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 23:51:49 by dateixei          #+#    #+#             */
-/*   Updated: 2022/04/02 02:26:13 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/04/02 12:34:51 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,25 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <string.h>
+
+int	ft_printf_d(int d)
+{
+	int		lenght;
+	
+	lenght = strlen(d);
+	write(1, &d, )
+}
+
+int	ft_check_arg(va_list arg, char c)
+{
+	int	lenght;
+
+	lenght = 0;
+	if(c == 'd')
+		ft_printf_d(va_arg(arg, int));
+		
+}
 
 int ft_printf(const char *str, ...)
 {
