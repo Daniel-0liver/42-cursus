50461b5b8dbf8493110941fa594e0a2393b9204e
Daniel-0liver <dtoliverbr@gmail.com>
2022-04-02 02:27:51 +0100

    function ft_printf

--- ft_printf/src/ft_printf.c
+++ ft_printf/src/ft_printf.c
@@ -6,29 +6,47 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 23:51:49 by dateixei          #+#    #+#             */
-/*   Updated: 2022/03/31 01:09:20 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/04/02 02:26:13 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include "ft_printf.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
 
 int ft_printf(const char *str, ...)
 {
-	char	*test;
-	
-	va_list args;
-	va_start(args, str);
-	test = va_arg(args, char);
-	printf("%s\n", test);
-	va_end(args);
-	return 0;
+	va_list		arg;
+	int			i;
+	int			lenght;
+	char		c;
+
+	i = 0;
+	lenght = 0;
+	va_start(arg, str);
+	while (str[i])
+	{
+		c = str[i + 1];
+		if (str[i] == '%')
+		{
+			printf("test\n");
+			lenght++;
+			i++;
+		}
+		else
+			lenght += write(1, &str[i], sizeof(char));
+		i++;
+	}
+	va_end(arg);
+	return (lenght);
 }
 
 int main()
 {
 	int i;
 
-	i = ft_printf("aas", "Testing");
+	i = ft_printf("%as%a%a");
+	printf("\n%d", i);
 	return 0;
 }
